7ca6c4e071c79724c253d2c4c5160d7e400e64af
aston7332 <aston.k@appwrk.eu>
2022-10-18 16:30:56 +0530

    CSRF token used and image file size code added

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const methodOverride = require('method-override');
 const tickets = require("./models/tickets");
 const multer = require('multer');
 const fs = require('fs');
+var cookieParser = require('cookie-parser')
+var csrf = require('csurf')
+var bodyParser = require('body-parser')
+
+// setup route middlewares
+var csrfProtection = csrf({ cookie: true })
+var parseForm = bodyParser.urlencoded({ extended: false })
+
 
 
 //image upload
@@ -21,24 +29,47 @@ var storage = multer.diskStorage({
 
 var upload = multer({
     storage: storage,
+    limits: {
+        fieldSize: 1024 * 1024 * 3,
+    },
+    fileFilter: function(req, file, callback){
+        validateFile(file, callback);
+    },
 }).single("image");
+
+
+var validateFile = function(file, cb ){
+    allowedFileTypes = /jpeg|jpg|png|gif/;
+    const extension = allowedFileTypes.test(file.originalname.toLowerCase());
+    const mimeType  = allowedFileTypes.test(file.mimetype);
+    if(extension && mimeType){
+      return cb(null, true);
+    }else{
+      cb("Invalid file type. Only JPEG, PNG and GIF file are allowed.")
+    }
+  }
 
 dotenv.config();
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(methodOverride('_method'));
 app.use(express.static("./images"));
+// parse cookies
+// we need this because "cookie" is true in csrfProtection
+app.use(cookieParser())
 
 
 mongoose.connect('mongodb://localhost:27017/tickets', { useNewUrlParser: true }, () => {
     console.log("Connected to db!");
 
-    app.get('/', (req, res) => {
-        res.render('tickets.ejs');
+    app.get('/', csrfProtection, (req, res) => {
+        
+        res.render('tickets.ejs' , { csrfToken: req.csrfToken() });
     });
 
     // Creating a POST request
-    app.post('/', upload, async (req, res) => {
+    app.post('/', upload, csrfProtection, async (req, res) => {
+        // console.log(req.file);
         const ticket = require("./models/tickets");
         const ticket1 = new ticket({
             task: req.body.task,
@@ -79,6 +110,8 @@ mongoose.connect('mongodb://localhost:27017/tickets', { useNewUrlParser: true },
     app.put('/ticketEdit/:id', upload, async (req, res) => {
         let myObjectId = mongoose.Types.ObjectId(req.params.id);
         let new_image = "";
+
+        
 
         if (req.file) {
             new_image = req.file.filename;
